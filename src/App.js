d5efdadc9a18c42555839955f8dd7180439d3cea
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 19:07:36 -0800

    experiment w/ ternary operator inside JSX

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 
 const App = ()  => {
   const name = 'John';
+  const isNameShowing = false;
 
   return (
     <div className="App">
-      <h1>Hello, {name}!</h1>
+      <h1>Hello, {isNameShowing ? 'John' : 'someone'}!</h1>
     </div>
   );
 }
