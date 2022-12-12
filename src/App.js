e749c62f1e8b9ea44d5deaab8d3a3ab3713f4925
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 19:52:31 -0800

    call useEffect

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useEffect } from 'react';
 const API_URL = 'http://www.omdbapi.com?apikey=96e2619f';
 
 const App = () => {
+    useEffect(() => {
+
+    }, []);
+    
     return (
         <h1>App</h1>
     );
