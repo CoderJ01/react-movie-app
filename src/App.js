71a60e755084c43a0fe30881c55f951eeea92cf4
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 19:55:57 -0800

    create function to fetch API info

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { useEffect } from 'react';
 const API_URL = 'http://www.omdbapi.com?apikey=96e2619f';
 
 const App = () => {
+
+    const searhMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+    }
+    
     useEffect(() => {
 
     }, []);
-    
+
     return (
         <h1>App</h1>
     );
