17a4e3a4d0d7fe345f11ed6913d634d932dd1ecf
CoderJ01 <buildingguy@sbcglobal.net>
2022-12-11 20:53:32 -0800

    statically render movie omn site

--- src/App.js
+++ src/App.js
@@ -33,6 +33,21 @@ const App = () => {
                     onClick={() => {}}
                 />
             </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}></img>
+                    </div>
+                    <div>
+                    <span>{movie1.Type}</span>
+                    <span>{movie1.Title}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
